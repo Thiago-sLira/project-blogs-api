208fe7f5f029e686997fd2c04f1484de1fad2069
Thiago Lira <thiago.slira@outlook.com>
2023-04-06 17:13:50 -0300

    Criado middleware para validar as entradas da requisição

---
+++ src/controllers/login.controller.js
@@ -0,0 +1,13 @@
+const loginService = require('../services/login.service');
+
+const userLogin = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+module.exports = {
+    userLogin,
+};
